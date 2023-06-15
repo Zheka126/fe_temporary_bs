27660dfda5e4e7c8af386e98f3bded1c697c4d8f
Constantin Danilov <Constantin.Danilov@endava.com>
2023-06-15 11:36:26 +0300

    fix: fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,28 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { useEffect, useState } from 'react';
 import MainPage from './pages/MainPage';
 import RegistrationPage from './pages/RegistrationPage';
-import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { useEffect, useState } from 'react';
 
-function App() {
+export const App = () => {
   const [state, setState] = useState(false);
-  let age = 12;
+  const age = 12;
 
   useEffect(() => {
     if (!state) setState(true);
-  }, []);
+  }, [state]);
 
   return (
-    <>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/registration" element={<RegistrationPage />} />
-            <Route index element={<MainPage />} />
-            {/* <Route path="*" element={<NoPage />} /> */}
-          </Routes>
-        </BrowserRouter>
-      </Provider>
-    </>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/registration" element={<RegistrationPage />} />
+          <Route index element={<MainPage />} />
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
-
-export default App;
