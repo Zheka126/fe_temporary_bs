ec6a891fffca345f80c3d47d1b0591092ee848b6
Constantin Danilov <Constantin.Danilov@endava.com>
2023-06-24 09:35:41 +0300

    feat: add simple validation for login

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,8 +3,13 @@ import { StyledButton } from './Button.styles';
 export interface ButtonProps {
   type: 'button' | 'submit' | 'reset';
   title: string;
+  disabled?: boolean;
 }
 
-export const Button = ({ type, title }: ButtonProps) => {
-  return <StyledButton type={type}>{title}</StyledButton>;
+export const Button = ({ type, title, disabled }: ButtonProps) => {
+  return (
+    <StyledButton type={type} disabled={disabled}>
+      {title}
+    </StyledButton>
+  );
 };
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 import { Button } from '../Button/Button';
 import {
   InputContainer,
+  StyledErrorMessage,
   StyledForm,
   StyledInput,
   Title,
 } from '../SignupForm/SignupForm.styles';
 import { ForgotPasswordLink, StyledParagraph } from './LoginForm.styles';
+import { loginValidation } from './loginValidation';
 
 export interface LoginValues {
   username: string;
@@ -20,13 +22,19 @@ const initialValues: LoginValues = {
   password: '',
 };
 
+const inputOutline = (
+  error: string | undefined,
+  touched: boolean | undefined
+) => (error && touched ? '1px solid red' : 'none');
+
 export const LoginForm = () => {
-  const onSubmit = (props: LoginValues) => {
-    console.log(props);
+  const onSubmit = (values: LoginValues) => {
+    console.log(values);
   };
 
-  const { handleSubmit, getFieldProps } = useFormik({
+  const { touched, errors, handleSubmit, getFieldProps } = useFormik({
     initialValues,
+    validationSchema: loginValidation,
     onSubmit,
   });
 
@@ -42,8 +50,12 @@ export const LoginForm = () => {
           id="username"
           type="text"
           placeholder="Enter username"
+          outline={inputOutline(errors.username, touched.username)}
           {...getFieldProps('username')}
         />
+        {touched.username && errors.username ? (
+          <StyledErrorMessage>{errors.username}</StyledErrorMessage>
+        ) : null}
       </InputContainer>
 
       <InputContainer>
@@ -52,13 +64,21 @@ export const LoginForm = () => {
           id="password"
           type="password"
           placeholder="Enter password"
+          outline={inputOutline(errors.password, touched.password)}
           {...getFieldProps('password')}
         />
+        {touched.password && errors.password ? (
+          <StyledErrorMessage>{errors.password}</StyledErrorMessage>
+        ) : null}
       </InputContainer>
 
       <ForgotPasswordLink to="">Forgot password?</ForgotPasswordLink>
 
-      <Button type="submit" title="Log in" />
+      <Button
+        type="submit"
+        title="Log in"
+        // disabled={Object.keys(errors).length}
+      />
     </StyledForm>
   );
 };
