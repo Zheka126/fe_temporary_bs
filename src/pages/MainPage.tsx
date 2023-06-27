e824eb1c32aa7bd30624b0c6cf5c3d9b701c86be
Constantin Danilov <Constantin.Danilov@endava.com>
2023-06-27 15:06:52 +0300

    fix: fix conflicts

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,16 @@
 import { BookFilter } from 'src/components/BookFilter/BookFilter';
 import { BookList } from 'src/components/BookList/BookList';
+import { Container } from 'src/styles';
 
 import { MainPageContainer } from './styles';
 
 export const MainPage = () => {
   return (
-    <MainPageContainer>
-      <BookList />
-      <BookFilter />
-    </MainPageContainer>
+    <Container>
+      <MainPageContainer>
+        <BookList />
+        <BookFilter />
+      </MainPageContainer>
+    </Container>
   );
 };
