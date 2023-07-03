c8c0f2baac98195317fb30b28fa355132425479f
Constantin Danilov <Constantin.Danilov@endava.com>
2023-07-03 20:25:59 +0300

    feat: set up json server

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import axios from "axios";
+import { useEffect } from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
-import { store } from './redux/store';
-import { openRoutes, privateRoutes } from './routes';
-import { theme } from './theme';
+import { store } from "./redux/store";
+import { openRoutes, privateRoutes } from "./routes";
+import { theme } from "./theme";
 
 export const App = () => {
+
+  const testAddObj = async () => {
+    await axios.post("http://localhost:3000/companies", {
+      name: "Endava"
+    });
+  }
+  const testDeleteObj = async () => {
+    await axios.delete("http://localhost:3000/companies/4");
+  }
+
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <Routes>
-            {[...openRoutes, ...privateRoutes].map(
-              ({ path, component: Component }) => {
-                return <Route key={path} path={path} element={<Component />} />;
-              }
-            )}
-          </Routes>
-        </ThemeProvider>
-      </BrowserRouter>
-    </Provider>
+    <>
+      <button type="button" onClick={testAddObj}>create</button>
+      <button type="button" onClick={testDeleteObj}>delete</button>
+      <Provider store={store}>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <Routes>
+              {[...openRoutes, ...privateRoutes].map(
+                ({ path, component: Component }) => {
+                  return (
+                    <Route key={path} path={path} element={<Component />} />
+                  );
+                }
+              )}
+            </Routes>
+          </ThemeProvider>
+        </BrowserRouter>
+      </Provider>
+    </>
   );
 };
