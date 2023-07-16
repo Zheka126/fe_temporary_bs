399a73ddc47c92eae74a21126ae9fe66bf827b19
Eugen Cucer <eugen.cucer@endava.com>
2023-07-16 08:31:27 +0000

    Feat/intpo002 pd 3716 book details page

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -2,6 +2,7 @@ export enum StatusCodes {
   SUCCESS = 200,
   CREATED = 201,
   BAD_REQUEST = 400,
+  NOT_ALLOWED = 405,
   INTERNAL_SERVER_ERROR = 500,
 }
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,17 @@
+import { AppRouter } from './AppRouter/AppRouter';
 import { AuthLayout } from './AuthLayout/AuthLayout';
 import { BookDetails } from './BookDetails/BookDetails';
 import { BookList } from './BookList/BookList';
 import { Button } from './common/Button/Button';
 import { Loader } from './common/Loader/Loader';
+import { Modal } from './common/Modal/Modal';
 import { Header } from './Header/Header';
 import { LoginForm } from './LoginForm/LoginForm';
 import { Pagination } from './Pagination/Pagination';
 import { SignupForm } from './SignupForm/SignupForm';
 
 export {
+  AppRouter,
   AuthLayout,
   BookDetails,
   BookList,
@@ -16,6 +19,7 @@ export {
   Header,
   Loader,
   LoginForm,
+  Modal,
   Pagination,
   SignupForm,
 };
